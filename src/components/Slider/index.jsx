4e15bafc1b30198d5990666a48ca7896ab997974
Aziz Gadzhiev <104369872+GreedisGood93@users.noreply.github.com>
2023-11-07 17:26:46 +0600

    feat: add-mui-design-card

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,52 +1,89 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from '@mui/material';
 
 export default function Slider() {
   const [weatherData, setWeatherData] = useState([]);
-  const [selectedDay, setSelectedDay] = useState(0);
+  const [currentLocation, setCurrentLocation] = useState({});
   //Call api
   useEffect(() => {
     axios
       .get(
-        `https://api.weatherapi.com/v1/forecast.json?key=cf5923fb527d4a26a9284517230711&q=Караганда&aqi=no&days=5&lang=ru
+        `https://api.weatherapi.com/v1/forecast.json?key=cf5923fb527d4a26a9284517230711&q=Караганда&aqi=no&days=7&lang=ru
         `,
       )
       .then((response) => {
-        console.log(response.data.forecast.forecastday[0].day);
+        console.log(response.data.location);
         setWeatherData(response.data.forecast.forecastday);
+        setCurrentLocation(response.data.location);
       });
   }, []);
-  const handleDayChange = (dayIndex) => {
-    setSelectedDay(dayIndex);
-  };
   return (
-    <div
-      style={{
-        textAlign: 'center',
-        margin: '50px auto',
+    <Box
+      sx={{
         display: 'flex',
-        flexDirection: 'row',
-        backgroundColor: 'teal',
-        color: 'wheat',
-        width: '600px',
-        minHeight: '400px',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        gap: '20px',
-        padding: '20px',
-        boxShadow: '11px 7px 20px 12px rgba(0, 0, 0, 0.2)',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        textAlign: 'center',
+        mt: '30px',
       }}
     >
-      {weatherData.map((item, index) => {
-        return (
-          <div key={index}>
-            <h3>{item.date}</h3>
-            <img src={item.day.condition.icon} alt="icon" />
-            <p>Средняя температура: {item.day.avgtemp_c}</p>
-            <p>{item.day.condition.text}</p>
-          </div>
-        );
-      })}
-    </div>
+      <Box>
+        <Typography variant="h2">{currentLocation.country}</Typography>
+        <Typography variant="h3">{currentLocation.name}</Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          textAlign: 'center',
+          mt: '30px',
+          gap: '5px',
+        }}
+      >
+        {weatherData.map((item, index) => {
+          return (
+            <Card
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                border: '1px solid black',
+                flexDirection: 'column',
+                alignItems: 'center',
+                padding: '10px',
+                maxHeight: '300px',
+                maxWidth: '200px',
+              }}
+              key={index}
+            >
+              <CardContent
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '5px',
+                }}
+              >
+                <Typography variant="span">{item.date}</Typography>
+                <img src={item.day.condition.icon} alt="icon" />
+                <Typography variant="p">
+                  Средняя температура: {item.day.avgtemp_c} °C
+                </Typography>
+                <Typography variant="p">{item.day.condition.text}</Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Подробнее</Button>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </Box>
+    </Box>
   );
 }
