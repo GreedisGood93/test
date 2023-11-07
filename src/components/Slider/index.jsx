e50b484682b0306c9520577412697ca8b5090011
Aziz Gadzhiev <104369872+GreedisGood93@users.noreply.github.com>
2023-11-07 16:19:54 +0600

    feat: add-call-api-and-view-data

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Slider() {
+  const [weatherData, setWeatherData] = useState([]);
+  const [selectedDay, setSelectedDay] = useState(0);
+  //Call api
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.weatherapi.com/v1/forecast.json?key=cf5923fb527d4a26a9284517230711&q=Караганда&aqi=no&days=5&lang=ru
+        `,
+      )
+      .then((response) => {
+        console.log(response.data.forecast.forecastday[0].day);
+        setWeatherData(response.data.forecast.forecastday);
+      });
+  }, []);
+  const handleDayChange = (dayIndex) => {
+    setSelectedDay(dayIndex);
+  };
   return (
     <div
       style={{
         textAlign: 'center',
         margin: '50px auto',
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
         backgroundColor: 'teal',
         color: 'wheat',
         width: '600px',
@@ -17,15 +34,19 @@ export default function Slider() {
         justifyContent: 'space-between',
         gap: '20px',
         padding: '20px',
-        borderRadius: '40%',
         boxShadow: '11px 7px 20px 12px rgba(0, 0, 0, 0.2)',
       }}
     >
-      <Typography variant="h3">22°C</Typography>
-      <Typography variant="h4">Влажность: 74%</Typography>
-      <Typography variant="h4">Переменная облачность</Typography>
-      <Typography variant="h4">Ветер: 32 км/ч</Typography>
-      <Typography variant="h2">London</Typography>
+      {weatherData.map((item, index) => {
+        return (
+          <div key={index}>
+            <h3>{item.date}</h3>
+            <img src={item.day.condition.icon} alt="icon" />
+            <p>Средняя температура: {item.day.avgtemp_c}</p>
+            <p>{item.day.condition.text}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
